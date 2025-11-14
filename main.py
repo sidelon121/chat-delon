da9b1a58da6b4caaee6b2eda8eb324a4d2a6b9dd
FARHAN AKMAL <akmalf416@gmail.com>
2025-11-14 21:34:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -249,5 +249,6 @@ def favicon():
     )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  
+    port = int(os.environ.get("PORT", 5000))
+    print(f"🚀 Server berjalan di port {port}")
     app.run(host="0.0.0.0", port=port)
